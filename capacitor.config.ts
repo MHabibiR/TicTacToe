c3c233a116cdc717bafb82d16897179a07054bdb
MHabibiR <rahmanhabibi517@gmail.com>
2026-05-03 19:05:54 +0700

    Publish Playstore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.Seirios.tictactoe',
-  appName: 'TicTacToe',
+  appName: 'Neon Arena',
   webDir: 'www'
 };
 
